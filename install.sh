0f181d058dafde216d740b0fef28aa2fb0933830
Daniel Ohlsson <dohlsson89@gmail.com>
2013-06-22 17:42:38 +0200

    Updated configuration files
    	modified:   bash/bash_profile
    	modified:   bash/bashrc
    	modified:   i3/config
    	modified:   install.sh
    	new file:   misc/Xresources
    	modified:   vim/vimrc

--- install.sh
+++ install.sh
@@ -2,4 +2,39 @@
 
 # This script will install symlinks to the config-files
 
-# WIP
+# install_file function
+# first argument is the target file
+# second argument is the source file
+install_file () {
+	if [ -a $1 ]
+	then
+		echo "$1 exists!"
+	else
+		echo "$1 added!"
+		ln -s $2 $1
+	fi
+}
+
+echo "Bash config files: "
+install_file $HOME/.bashrc $PWD/bash/bashrc
+install_file $HOME/.bash_profile $PWD/bash/bash_profile
+echo -e "\n"
+
+echo "Vim config files: "
+install_file $HOME/.vimrc $PWD/vim/vimrc
+echo -e "\n"
+
+echo "i3 config files: "
+if [ ! -d $HOME/.i3 ]
+then
+	echo "Created $HOME/.i3/ directory"
+	mkdir $HOME/.i3
+fi
+install_file $HOME/.i3/config $PWD/i3/config
+install_file $HOME/.i3status.conf $PWD/i3/i3status.conf
+echo -e "\n"
+
+echo "Misc. config files: "
+install_file $HOME/.Xresources $PWD/misc/Xresources
+install_file $HOME/.xinitrc $PWD/misc/xinitrc
+echo -e "\n"
