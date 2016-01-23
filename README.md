d11c3114c45f78c0cbf8438144561a9509d9aadb
Daniel Ohlsson <dohlsson89@gmail.com>
2016-01-23 21:04:48 +0100

    Minor changes

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ configs
 =======
 
 My config files
+
--- install.sh
+++ install.sh
@@ -18,11 +18,11 @@ install_file () {
 echo "Bash config files: "
 install_file $HOME/.bashrc $PWD/bash/bashrc
 install_file $HOME/.bash_profile $PWD/bash/bash_profile
-echo -e "\n"
+echo
 
 echo "Vim config files: "
 install_file $HOME/.vimrc $PWD/vim/vimrc
-echo -e "\n"
+echo
 
 echo "i3 config files: "
 if [ ! -d $HOME/.i3 ]
@@ -32,12 +32,11 @@ then
 fi
 install_file $HOME/.i3/config $PWD/i3/config
 install_file $HOME/.i3status.conf $PWD/i3/i3status.conf
-echo -e "\n"
+echo
 
 echo "Misc. config files: "
 install_file $HOME/.Xresources $PWD/misc/Xresources
 install_file $HOME/.Xdefaults $PWD/misc/Xresources
 install_file $HOME/.Xmodmap $PWD/misc/Xmodmap
 install_file $HOME/.xinitrc $PWD/misc/xinitrc
-install_file $HOME/.asoundrc $PWD/misc/asoundrc
-echo -e "\n"
+echo
