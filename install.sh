541832e7af685160bd3cfb97f719a40fb9caeb3f
Daniel Ohlsson <dohlsson89@gmail.com>
2018-12-23 12:44:09 +0100

    Moved PATHs into .profile

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # This script will install symlinks to the config-files
 
 # install_file function, basically a wrapper for ln with prints
@@ -20,38 +22,28 @@ install_file () {
 
 echo "Bash config files: "
 install_file $HOME/.bashrc $PWD/bash/bashrc
-install_file $HOME/.bash_profile $PWD/bash/bash_profile
 install_file $HOME/.inputrc $PWD/bash/inputrc
 echo
 
 echo "Vim config files: "
-if [ ! -d $HOME/.vim/colors/ ]
-then
-	echo "Created $HOME/.vim/colors/ directory"
-	mkdir -p $HOME/.vim/colors/
-fi
+mkdir -p $HOME/.vim/colors/
 install_file $HOME/.vimrc $PWD/vim/vimrc
 install_file $HOME/.vim/colors/mycolors.vim $PWD/vim/colors/mycolors.vim
 echo
 
 echo "i3 config files: "
-if [ ! -d $HOME/.i3 ]
-then
-	echo "Created $HOME/.i3/ directory"
-	mkdir $HOME/.i3
-fi
+mkdir -p $HOME/.i3
 install_file $HOME/.i3/config $PWD/i3/config
 install_file $HOME/.i3status.conf $PWD/i3/i3status.conf
 echo
 
 echo "Misc. config files: "
+install_file $HOME/.profile $PWD/misc/profile
+install_file $HOME/.xsessionrc $PWD/misc/xsessionrc
 install_file $HOME/.Xresources $PWD/misc/Xresources
 install_file $HOME/.Xdefaults $PWD/misc/Xresources
-install_file $HOME/.Xmodmap $PWD/misc/Xmodmap
 install_file $HOME/.xinitrc $PWD/misc/xinitrc
 install_file $HOME/.screenrc $PWD/misc/screenrc
-
 mkdir -p $HOME/.config/xfce4/terminal
 install_file $HOME/.config/xfce4/terminal/terminalrc $PWD/config/xfce4/terminal/terminalrc
-
 echo
